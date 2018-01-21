0a7e0024338d18eaffb9c43a67b31447ceb1e127
Fernando Mosbacher Azevedo <azevedo.fma@gmail.com>
2018-01-21 12:47:45 -0200

    Removed static path

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ app.set('port', process.env.PORT || 3000)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(__dirname, 'public')))
-
 // Routes
 app.use('/', indexRoute)
 app.use('/api/pokemon', pokemonRoutes)
