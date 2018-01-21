de361279d25ff74f41d04bbf177ffcaa2695cf94
Fernando Mosbacher Azevedo <azevedo.fma@gmail.com>
2018-01-21 12:44:32 -0200

    Adding html page

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set('port', process.env.PORT || 3000)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 // Routes
 app.use('/', indexRoute)
 app.use('/api/pokemon', pokemonRoutes)
